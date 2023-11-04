e4cfdeaffbd2ea371814d37ad6fbce6e8a04e924
frailin02 <108911322+frailin02@users.noreply.github.com>
2023-11-04 01:32:26 -0300

    Add files via upload

---
+++ php-code/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Docker Compose</title>
+</head>
+<body>
+    <h1>Frailin Ogando Bido 20211690</h1>
+    <h4>Docker compose</h4>
+    <?php
+        echo "<h2><bold>jaja</bold></h2>";
+    ?>
+</body>
+</html>
